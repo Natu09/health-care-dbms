4b6c589fe7ff1f82c2f25f059e42fe05bd0ecc33
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2020-03-14 17:14:51 -0600

    changed route to Calendar.jsx instead of table

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,23 +1,6 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.jsx";
 import UserProfile from "views/UserProfile.jsx";
-import TableList from "views/TableList.jsx";
+import Calendar from "views/Calendar.jsx";
 import Typography from "views/Typography.jsx";
 import Icons from "views/Icons.jsx";
 import Maps from "views/Maps.jsx";
@@ -40,11 +23,10 @@ const dashboardRoutes = [
     layout: "/admin"
   },
   {
-    path: "/table",
-    name: "Table List",
-    icon: "pe-7s-note2",
-    component: TableList,
-    layout: "/admin"
+    path: "/calendar",
+    name: "Calendar",
+    icon: "pe-7s-date",
+    component: Calendar
   },
   {
     path: "/typography",
---
+++ frontend/src/views/Calendar.jsx
@@ -0,0 +1,14 @@
+import { Calendar } from "@fullcalendar/core";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+export default () => {
+  document.addEventListener("DOMContentLoaded", function() {
+    var calendarEl = document.getElementById("calendar");
+
+    var calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin]
+    });
+
+    calendar.render();
+  });
+};
