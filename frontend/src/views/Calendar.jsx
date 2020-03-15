e504dde8108747a74016dca854e956b649019b9e
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2020-03-15 12:44:35 -0600

    removed unnnesecary headers

--- frontend/src/views/Calendar.jsx
+++ frontend/src/views/Calendar.jsx
@@ -1,14 +1,22 @@
-import { Calendar } from "@fullcalendar/core";
+// import { Calendar } from "@fullcalendar/core";
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+import "@fullcalendar/list/main.css";
 
 export default () => {
-  document.addEventListener("DOMContentLoaded", function() {
-    var calendarEl = document.getElementById("calendar");
-
-    var calendar = new Calendar(calendarEl, {
-      plugins: [dayGridPlugin]
-    });
-
-    calendar.render();
-  });
+  return (
+    <FullCalendar
+      defaultView="dayGridMonth"
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+      handleWindowResize={true}
+      contentHeight={625}
+    />
+  );
 };
