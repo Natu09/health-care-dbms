b4fb18a3bd567484fa9704514ca839a89e45d072
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2020-03-16 17:49:53 -0600

    "add a day, week, month, and list view and fixed outdated bootstrap"

--- frontend/src/views/Calendar.jsx
+++ frontend/src/views/Calendar.jsx
@@ -5,6 +5,7 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+// import bootstrapPlugin from "@fullcalendar/bootstrap";
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
@@ -14,8 +15,14 @@ export default () => {
   return (
     <FullCalendar
       defaultView="dayGridMonth"
+      header={{
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+      }}
       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
       handleWindowResize={true}
+      contentHeight={625}
       themeSyste="bootstrap"
     />
   );
