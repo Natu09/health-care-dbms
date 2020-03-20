7fb6fd6882ef12758bca9da9b98529e942c9b592
Lucas Ramos-Strankman <lucasrstrankman@gmail.com>
2020-03-19 19:08:46 -0600

    Rewrote some of Calendar to better work with

--- frontend/src/views/Calendar.jsx
+++ frontend/src/views/Calendar.jsx
@@ -10,13 +10,84 @@ import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import "@fullcalendar/list/main.css";
 
-export default () => {
-  return (
-    <FullCalendar
-      defaultView="dayGridMonth"
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
-      handleWindowResize={true}
-      themeSyste="bootstrap"
-    />
-  );
-};
+
+export default class Calendar extends React.Component {
+   //calendarComponentRef = React.createRef();
+  constructor(props){
+    super(props)
+
+    const options = {
+      defaultView: "dayGridMonth",
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      },
+
+      plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
+      ref:  this.calendarComponentRef,
+      //weekends={ this.state.calendarWeekends }
+      events: [ // initial event data
+        {  
+          title: 'Dr. Wilson', 
+          start: new Date().setHours(15), 
+          end: new Date()//"2020-03-20:00:00",
+        }
+      ],
+      eventColor: '#378006',
+      displayEventEnd: true,
+      dateClick: this.handleDateClick,
+    }
+    this.state = {
+      options
+    };
+  }
+
+
+  render() {
+    const { options } = this.state;
+    return (
+      <div className='calendar'>
+        <FullCalendar {...options}/>
+      </div>
+
+    )
+  }
+
+  // toggleWeekends = () => {
+  //   this.setState({ // update a property
+  //     calendarWeekends: !this.state.calendarWeekends
+  //   })
+  // }
+
+  // gotoPast = () => {
+  //   let calendarApi = this.calendarComponentRef.current.getApi()
+  //   calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+  // }
+
+  // handleDateClick = (arg) => {
+  //   if (true) {
+  //     this.setState({  // add new event data
+  //       calendarEvents: this.state.calendarEvents.concat({ // creates a new array
+  //         title: 'New Event',
+  //         start: arg.date,
+  //         allDay: arg.allDay
+  //       })
+  //     })
+  //   }
+  // }
+
+}
+// function Calendar ({...props}){
+//   return (
+//     <FullCalendar
+//       defaultView="dayGridMonth"
+//       plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
+//       handleWindowResize={true}
+//       themeSyste="bootstrap"   
+//     />
+   
+//   );
+// }
+
+// export default Calendar;
