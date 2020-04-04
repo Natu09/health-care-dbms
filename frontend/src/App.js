2b99dbdde9588fc9caf68c140b21945686340719
Nathaniel <nathaniel.habtegerge@ucalgary.ca>
2020-04-04 14:40:56 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
