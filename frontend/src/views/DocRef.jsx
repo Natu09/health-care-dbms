3208a18654045219fe27a1d18ea44726eee4706c
justinf34 <justin.flores@ucalgary.ca>
2020-04-12 23:00:59 -0600

    Co-authored-by: Nathaniel Habtegergesa <Nathaniel.habtegerge@ucalgary.ca> Allow doctor to give refrences to patients but needs testing

--- frontend/src/views/DocRef.jsx
+++ frontend/src/views/DocRef.jsx
@@ -1,3 +1,4 @@
+// Imports
 import React, { Component } from "react";
 
 import {
@@ -12,22 +13,27 @@ import {
 } from "react-bootstrap";
 
 import { Card } from "components/Card/Card.jsx";
+import NotificationSystem from "react-notification-system";
+import { style } from "../variables/Variables";
+
 import { AuthContext } from "../Auth";
 import { db } from "../firebase";
 
-const tableHeader = [
-  "Patient ID",
-  "Patient Name",
-  "Doctor ID",
-  "Doctor Name",
-  "Test Type",
-];
-
+/**
+ * React component that renders the references page for
+ * doctor users. Also contains the function that achieves
+ * the functionality needed to give refrences and view them
+ *
+ * @author Justin Flores, Nathaniel Habtegergesa
+ * @since 1.0
+ */
 export class DocRef extends Component {
-  static contextType = AuthContext;
+  static contextType = AuthContext; // To access current user ID
 
   constructor(props) {
     super(props);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleAlert = this.handleAlert.bind(this);
     this.state = {
       patients: [],
       doctors: [],
@@ -35,27 +41,92 @@ export class DocRef extends Component {
       doctor: "",
       test: "Blood Test",
       docRefs: [],
+      _notificationSystem: null,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  /**
+   * Shows an alert to notify the user if their actions
+   * were successful or not
+   * @param {string} position where the notification will appear
+   * @param {striing} level  success or not
+   * @param {string} message Message in the alert
+   */
+  handleAlert(position, level, message) {
+    const logo = level === "error" ? "pe-7s-attention" : "pe-7s-like2";
+    const title = level === "error" ? "Oops!" : "Yay!";
+
+    this.state._notificationSystem.addNotification({
+      title: <span data-notify="icon" className={logo}></span>,
+      message: (
+        <div>
+          <h1>{title}</h1>
+          <p>{message}</p>
+        </div>
+      ),
+      level: level,
+      position: position,
+      autoDismiss: 2,
+    });
+  }
+
+  /**
+   * Changes state variables when user changes options
+   * in the input forms
+   * @param {string} name
+   * @param {event object} event
+   */
   handleChange(name, event) {
     this.setState({ [name]: event.target.value });
   }
 
+  /**
+   * Adds a refrence to the DB and add
+   * the new refrence to the table
+   * @param {event object} event
+   */
   handleSubmit(event) {
     event.preventDefault();
     const cont = this.context;
 
-    db.collection("Reference").add({
-      fromDoctorID: cont.currentUser.uid,
-      patientID: this.state.patient,
-      toDoctorID: this.state.doctor,
-      type: this.state.test,
-    });
+    // Check if patient already has that same type of reference from
+    // the same doctor
+    for (let i = 0; i < this.state.docRefs.length; i++) {
+      if (
+        this.state.test === this.state.docRefs[i].type &&
+        this.state.test === this.state.docRefs[i].patientID
+      ) {
+        this.handleAlert("tc", "error", "Patient already has that reference!");
+        return;
+      }
+    }
+
+    db.collection("Reference")
+      .add({
+        fromDoctorID: cont.currentUser.uid,
+        patientID: this.state.patient,
+        toDoctorID: this.state.doctor,
+        type: this.state.test,
+      })
+      .then((res) => {
+        const new_ref = {
+          id: res.id,
+          patientID: this.state.patient,
+          toDoctorID: this.state.doctor,
+          type: this.state.test,
+        };
+        this.setState({ docRefs: this.state.docRefs.concat(new_ref) });
+        this.handleAlert("tc", "success", "Referrence given!");
+      });
   }
 
+  /**
+   * Retrieves all patients in the DB and
+   * add their info in the patient state
+   * variable
+   */
   getPatients() {
     const arr = [];
     db.collection("Users")
@@ -81,6 +152,10 @@ export class DocRef extends Component {
       });
   }
 
+  /**
+   * Retrieve all doctors in the DB and
+   * add them to the doctor state variable
+   */
   getDoctors() {
     const arr = [];
     db.collection("Users")
@@ -97,7 +172,7 @@ export class DocRef extends Component {
           arr.push(doctor);
         });
       })
-      .then(() => {
+      .then((_) => {
         this.setState({ doctors: arr });
         this.setState({
           doctor: this.state.doctors[0].id,
@@ -106,6 +181,10 @@ export class DocRef extends Component {
       });
   }
 
+  /**
+   * Get all refernces that the current doctor has
+   * given to patients
+   */
   getReferrences() {
     const cont = this.context;
     const refs = [];
@@ -114,7 +193,9 @@ export class DocRef extends Component {
       .get()
       .then((res) => {
         res.forEach((ref) => {
-          refs.push(ref.data());
+          const reference = ref.data();
+          reference.id = ref.id;
+          refs.push(reference);
         });
       })
       .then((_) => {
@@ -123,9 +204,12 @@ export class DocRef extends Component {
   }
 
   componentDidMount() {
-    this.getPatients();
-    this.getDoctors();
-    this.getReferrences();
+    // Initialize state variables
+    this.getPatients(); // Initialize patients
+    this.getDoctors(); // Initialize doctors
+    this.getReferrences(); // Initialize patients
+
+    this.setState({ _notificationSystem: this.refs.notificationSystem });
   }
 
   render() {
@@ -137,6 +221,8 @@ export class DocRef extends Component {
           height: "100vh",
         }}
       >
+        <NotificationSystem ref="notificationSystem" style={style} />
+
         <Grid container direction="row" justify="center" alignItems="center">
           <Row>
             <Card
@@ -211,6 +297,7 @@ export class DocRef extends Component {
                 <Table>
                   <thead variant="dark">
                     <tr>
+                      <th>Reference ID</th>
                       <th>Patient ID</th>
                       <th>Doctor ID</th>
                       <th>Test</th>
@@ -220,6 +307,7 @@ export class DocRef extends Component {
                     {this.state.docRefs.map((ref, key) => {
                       return (
                         <tr key={key}>
+                          <td key={key}>{ref.id}</td>
                           <td key={key}>{ref.patientID}</td>
                           <td key={key}>{ref.toDoctorID}</td>
                           <td key={key}>{ref.type}</td>
